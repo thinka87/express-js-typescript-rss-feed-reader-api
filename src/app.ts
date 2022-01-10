3031f4300c7d1c5492a3970801eecc769b6f60d3
thinka87 <thinkaemoney@gmail.com>
2022-01-10 16:46:19 +0530

    remove comments

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,7 @@ import Logger from "../src/utils/logger";
 async function startServer() {
   const app = express();
 
-  /**
-   * A little hack here
-   * Import/Export can only be used in 'top-level code'
-   * Well, at least in node 10 without babel and at the time of writing
-   * So we are using good old require.
-   **/
+ 
   await require("./loaders").default({ expressApp: app });
 
   app
